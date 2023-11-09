ffa085a099687a42ade0a2ee1f501930582bc8f5
= <=>
2023-11-09 10:41:52 +0100

    Add auth condition for broadcasting

--- routes/channels.php
+++ routes/channels.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('notifications.{id}', function ($user, $id) {
-    return true;
-});
-Broadcast::channel('notifications', function () {
-    return true;
+    return $user->id === +$id;
 });
